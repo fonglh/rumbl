c8c8ba2550ee9e9ab41f2d8c203685002b9eb0b8
fonglh <fonglh@gmail.com>
2016-09-30 22:06:06 +0800

    Seed category data.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Rumbl.Repo
+alias Rumbl.Category
+
+# Seed some categories, these won't change often so there's no need
+# for to create controller and views to manage them.
+for category <- ~w(Action Drama Romance Comedy Sci-fi) do
+  # Check if the category exists before creating it
+  Repo.get_by(Category, name: category) ||
+    Repo.insert!(%Category{name: category})
+end

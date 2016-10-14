f448e3a25cf291b9b1284f329d77bf125a80a994
fonglh <fonglh@gmail.com>
2016-10-14 22:31:10 +0800

    Annotate videos.
    Remove ping functionality.
    
    Update video.js to push a message to the channel when an annotation is
    posted. Render an annotation when the message is received. This can be
    from other clients too.
    
    Update Video channel to broadcast the new_annotation event to other
    clients when it receives the message from a client.

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -3,27 +3,25 @@ defmodule Rumbl.VideoChannel do
 
   # Clients can join topics on a channel
   # Return {:ok, socket} to authorize a join attempt or {:error, socket} to deny one.
-  #
-  # Add video ID from topic to socket.assigns, which typically holds a map.
   def join("videos:" <> video_id, _params, socket) do
-    :timer.send_interval(5_000, :ping)
-    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+    {:ok, socket}
   end
 
-  # callback for receiving direct channel events.
-  # invoked whenever an elixir message reaches the channel.
-  # match on :ping and increment counter when it arrives.
-  # Takes a socket and returns a transformed socket.
-  def handle_info(:ping, socket) do
-    count = socket.assigns[:count] || 1
+  # handles all incoming messages to the channel pushed from the remote client.
+  # not persisting to the database yet, so just broadcast new_annotation events
+  # to all clients on this topic.
+  def handle_in("new_annotation", params, socket) do
+    # the payload is an arbitrary map.
+    # we can send as many messages as we like.
+    # DO NOT forward along the raw payload.
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    }
 
-    # push the ping event, picked up by client with the
-    # channel.on(event, callback) API. See video.js.
-    push socket, "ping", %{count: count}
-
-    # return tagged tuple.
-    # :noreply means not sending a reply, assign function transforms
-    # the socket by adding the new count
-    {:noreply, assign(socket, :count, count + 1)}
+    # sends reply back to the client with status and socket
+    # can also use :noreply
+    {:reply, :ok, socket}
   end
 end
--- web/static/js/video.js
+++ web/static/js/video.js
@@ -23,12 +23,48 @@ let Video = {
     // this will be used to connect ES6 client to Phoenix VideoChannel
     // convention for topic identifiers
     let vidChannel = socket.channel("videos:" + videoId)
-    vidChannel.on("ping", ({count}) => console.log("PING", count) )
+
+    // Handle click event on the post button.
+    // push function on vidChannel sends contents of message to server.
+    // then clear the input control.
+    postButton.addEventListener("click", e => {
+      let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+      // when we push a message to the server, we can opt to receive a response
+      vidChannel.push("new_annotation", payload)
+                .receive("error", e => console.log(e) )
+      msgInput.value = ""
+    })
+
+    // when users post new annotations, the server will broadcast the event to
+    // the clients. This handles the event.
+    vidChannel.on("new_annotation", (resp) => {
+      this.renderAnnotation(msgContainer, resp)
+    })
 
     // create a new channel object from our socket and give it a topic
     vidChannel.join()
       .receive("ok", resp => console.log("joined the video channel", resp) )
       .receive("error", reason => console.log("join failed", reason) )
+  },
+
+  // escape user input before injecting values into the page.
+  esc(str){
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str))
+    return div.innerHTML
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}){
+    // build a DOM node with the user's name and annotation body
+    let template = document.createElement("div")
+    template.innerHTML = `
+    <a href="#" data-seek="${this.esc(at)}">
+      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
+     `
+    // append it to the msgContainer list and scroll to the right point.
+    msgContainer.appendChild(template)
+    msgContainer.scrollTop = msgContainer.scrollHeight
   }
 }
 export default Video

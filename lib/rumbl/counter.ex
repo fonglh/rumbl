7aecdf11a35625e1b4506e30af175c386f2faad6
fonglh <fonglh@gmail.com>
2016-10-19 22:49:00 +0800

    Add failure handling for Counter.
    Add Counter as a worker in the supervision tree.
    Make it crash when the value goes to 0 and watch it restart.

--- lib/rumbl/counter.ex
+++ lib/rumbl/counter.ex
@@ -16,7 +16,20 @@ defmodule Rumbl.Counter do
 
   # Functions below run in the server
   def init(initial_val) do
+    # send itself a :tick message every second
+    Process.send_after(self, :tick, 1000)
     {:ok, initial_val}
+  end
+
+  # crash the counter when it goes negative
+  def handle_info(:tick, val) when val <=0, do: raise "boom!"
+
+  # process the :tick message
+  def handle_info(:tick, val) do
+    IO.puts "tick #{val}"
+    Process.send_after(self, :tick, 1000)
+    # count down
+    {:noreply, val - 1}
   end
 
   def handle_cast(:inc, val) do

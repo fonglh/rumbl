61d795c1e11e81c0e4b9144ed16a39d66b78fad4
fonglh <fonglh@gmail.com>
2016-10-29 11:32:48 +0800

    Use Process monitors to handle failures.
    Don't wait forever for a process that's already crashed.

--- lib/rumbl/info_sys.ex
+++ lib/rumbl/info_sys.ex
@@ -20,12 +20,43 @@ defmodule Rumbl.InfoSys do
 
     backends
     |> Enum.map(&spawn_query(&1, query, limit))
+    |> await_results(opts)
+    |> Enum.sort(&(&1.score >= &2.score))
+    |> Enum.take(limit)
   end
 
   defp spawn_query(backend, query, limit) do
     query_ref = make_ref()
     opts = [backend, query, query_ref, self(), limit]
     {:ok, pid} = Supervisor.start_child(Rumbl.InfoSys.Supervisor, opts)
-    {pid, query_ref}
+    monitor_ref = Process.monitor(pid)
+    {pid, monitor_ref, query_ref}
+  end
+
+  defp await_results(children, _opts) do
+    await_result(children, [], :infinity)
+  end
+
+  defp await_result([head|tail], acc, timeout) do
+    {pid, monitor_ref, query_ref} = head
+
+    receive do
+      # valid result, drop the monitor.
+      # [:flush] guarantees that the :DOWN message is removed from the inbox
+      # in case it's delivered before we drop the monitor.
+      {:results, ^query_ref, results} ->
+        Process.demonitor(monitor_ref, [:flush])
+        await_result(tail, results ++ acc, timeout)
+      # match on monitor_ref, because :DOWN messages come from the monitor
+      # and not the GenServer.
+      # Recurse without adding to the accumulator.
+      {:DOWN, ^monitor_ref, :process, ^pid, _reason} ->
+        await_result(tail, acc, timeout)
+    end
+  end
+
+  # base case, ends recursion when list has been processed.
+  defp await_result([], acc, _) do
+    acc
   end
 end
